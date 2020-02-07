314d3cdff65f7067ab2a43cbc2949dfd423e4ca3
NakagamiYuta <guguru0014@gmail.com>
2020-02-07 23:57:19 +0900

    [add] 描画の幅/高さを設定するUIに動作を実装した

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,10 +15,13 @@ public class MainUIController implements Initializable {
 
     // FXML
     @FXML
-    AnchorPane areaChartPane;
+    private AnchorPane areaChartPane;
+    @FXML
+    private TextField depthVal, upperHeightVal, lowerHeightVal, upperWidthVal, lowerWidthVal;
 
     // 描画用
-    NegativeBGAreaChart seabedChart;
+    private NegativeBGAreaChart seabedChart;
+    private double depth, upperHeight, lowerHeight, upperWidth, lowerWidth;
 
     /**
      * fxml.Initializable
@@ -29,7 +33,29 @@ public class MainUIController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resource) {
         // 初期化
+        upperWidth = 350.0;
+        lowerWidth = 0.0;
+        upperHeight = 20.0;
+        lowerHeight = -20.0;
         initAreaChart();
+
+        // UI部品の動作を実装
+        upperWidthVal.textProperty().addListener((obs, oldText, newText) -> {
+            upperWidth = loadInputValue(upperWidthVal);
+            initAreaChart();
+        });
+        lowerWidthVal.textProperty().addListener((obs, oldText, newText) -> {
+            lowerWidth = loadInputValue(lowerWidthVal);
+            initAreaChart();
+        });
+        upperHeightVal.textProperty().addListener((obs, oldText, newText) -> {
+            upperHeight = loadInputValue(upperHeightVal);
+            initAreaChart();
+        });
+        lowerHeightVal.textProperty().addListener((obs, oldText, newText) -> {
+            lowerHeight = loadInputValue(lowerHeightVal);
+            initAreaChart();
+        });
     }
 
     /**
@@ -40,7 +66,13 @@ public class MainUIController implements Initializable {
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Distance(km)");
+        xAxis.setAutoRanging(false);
+        xAxis.setLowerBound(lowerWidth);
+        xAxis.setUpperBound(upperWidth);
         yAxis.setLabel("Height(m)");
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(lowerHeight);
+        yAxis.setUpperBound(upperHeight);
 
         // AreaChart
         seabedChart = new NegativeBGAreaChart<>(xAxis, yAxis);
@@ -54,6 +86,17 @@ public class MainUIController implements Initializable {
         AnchorPane.setBottomAnchor(seabedChart, 10.0);
         areaChartPane.getChildren().clear();
         areaChartPane.getChildren().add(seabedChart);
+    }
+
+    /**
+     * TextFieldから値を読み取って返す
+     */
+    private double loadInputValue(TextField tf) {
+        try {
+            return Double.parseDouble(tf.getText());
+        } catch (Exception e) {
+            return 0.0;
+        }
     }
 
 }
