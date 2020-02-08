11d730af871494679361e1d1baf32d1e6777be26
NakagamiYuta <guguru0014@gmail.com>
2020-02-08 22:59:48 +0900

    [add] SeabedDataクラスにComparableインタフェースを実装した

--- src/controller/SeabedData.java
+++ src/controller/SeabedData.java
@@ -1,11 +1,20 @@
 package controller;
 
-class SeabedData {
+import java.lang.Comparable;
+
+class SeabedData implements Comparable {
 
     public double dist, depth;
 
     public SeabedData(double dist, double depth) {
         this.dist = dist;
         this.depth = depth;
+    }
+
+    public int compareTo(Object other) {
+        SeabedData otherData = (SeabedData)other;
+        if(dist == otherData.dist)
+            return 0;
+        return dist < otherData.dist ? -1 : 1;
     }
 }
