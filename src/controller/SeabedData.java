4a3132a63a62d85c97b2ddfaafc2a83e31b89e11
NakagamiYuta <guguru0014@gmail.com>
2020-02-08 20:29:24 +0900

    [add] SeabedDataクラス定義

---
+++ src/controller/SeabedData.java
@@ -0,0 +1,11 @@
+package controller;
+
+class SeabedData {
+
+    public double dist, height;
+
+    public SeabedData(double dist, double height) {
+        this.dist = dist;
+        this.height = height;
+    }
+}
