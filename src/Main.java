69fc2954bbb6f6b748f2ecb0cdd4b18c899684d9
NakagamiYuta <guguru0014@gmail.com>
2020-02-19 22:40:37 +0900

    [add] .propertiesファイルをもとに描画を行うようにした

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import javafx.stage.Stage;
 import javafx.stage.Screen;
 import javafx.geometry.Rectangle2D;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import controller.MainUIController;
 
 public class Main extends Application {
@@ -20,10 +25,16 @@ public class Main extends Application {
         int width = (int)Math.min(1280, d.getWidth());
         int height = (int)Math.min(720, d.getHeight());
 
+        // Property
+        URL propURLs[] = {getClass().getResource("/fxml/locale/")};
+        URLClassLoader urlLoader = new URLClassLoader(propURLs);
+
         // Scene
         Scene scene = null;
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainUI.fxml"));
+            Locale locale = Locale.getDefault();
+            ResourceBundle resource = ResourceBundle.getBundle("locale", locale, urlLoader);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainUI.fxml"), resource);
             loader.setController(new MainUIController());
             scene = new Scene(loader.load(), width, height);
         } catch (Exception e) {
